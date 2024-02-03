dff87e6f75179d434422f7e64835a8f8d2a03bdb
Camilo Gil <camilo.gil27071997@gmail.com>
2024-02-03 03:27:22 -0500

    Se organizar home y main para que funciones bien el routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MainComponent } from './components/main/main.component';
+//import { authGuard } from './guard/auth/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    // canActivate:[authGuard],
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'songs',
+        loadChildren: () =>
+          import('./module/songs/songs.module').then((m) => m.SongsModule),
+      },
+    ],
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./module/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { LoginComponent } from './module/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LoginComponent,
+    HomeComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule
-    
   ],
   providers: [],
   bootstrap: [AppComponent]
